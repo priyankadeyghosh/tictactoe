314cee979ff4c26aa51471418988522f2eeff412
priyankadeyghosh <74904505+priyankadeyghosh@users.noreply.github.com>
2021-06-12 10:38:07 +0530

    Add files via upload

--- tictaco.js
+++ tictaco.js
@@ -1,5 +1,8 @@
 player=1
 
+scoreX=0;
+scoreY=0;
+
 
 function f1(){
 if (player===1){
@@ -136,6 +139,11 @@ function play(){
     b7=document.getElementById("b7").value;
     b8=document.getElementById("b8").value;
     b9=document.getElementById("b9").value;
+    document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+           
+    document.getElementById('p2').innerHTML="player O score:"+ scoreY;
+
+           
       
 
    if((b1==="X")&&(b2==="X")&&(b3==="X")){
@@ -146,175 +154,329 @@ document.getElementById("b7").disabled=true;
 document.getElementById("b8").disabled=true;
 document.getElementById("b9").disabled=true;
 //window.alert("player X won");
-document.getElementById('msg')
-            .innerHTML = "Player X won";
+document.getElementById('msg').innerHTML = "Player X won";
+scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+           
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
+    
+           
+
+    
 
    }
 
-if((b4==="X")&&(b5==="X")&&(b6==="X")){
+else if((b4==="X")&&(b5==="X")&&(b6==="X")){
 document.getElementById("b1").disabled=true;
 document.getElementById("b2").disabled=true;
 document.getElementById("b3").disabled=true;
 document.getElementById("b7").disabled=true;
 document.getElementById("b8").disabled=true;
 document.getElementById("b9").disabled=true;
-window.alert("player X won");
+document.getElementById('msg')
+            .innerHTML = "Player X won";
+
+
+scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
+        
+            //d//ocument.getElementById('p1')
+           /// .innerHTML = "player X score:"+score;
+
+           //; document.getElementById('p2')
+            //.innerHTML = "player O score:"+score;     
 
     }
 
-if((b7==="X")&&(b8==="X")&&(b9==="X")){
+else if((b7==="X")&&(b8==="X")&&(b9==="X")){
 document.getElementById("b1").disabled=true;
 document.getElementById("b2").disabled=true;
 document.getElementById("b3").disabled=true;
 document.getElementById("b4").disabled=true;
 document.getElementById("b5").disabled=true;
 document.getElementById("b6").disabled=true;
-window.alert("player X won");
+document.getElementById('msg')
+            .innerHTML = "Player X won";
+            scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
 
     }   
     
-if((b1==="X")&&(b4==="X")&&(b7==="X")){
+else if((b1==="X")&&(b4==="X")&&(b7==="X")){
 document.getElementById("b2").disabled=true;
 document.getElementById("b3").disabled=true;
 document.getElementById("b5").disabled=true;
 document.getElementById("b6").disabled=true;
 document.getElementById("b8").disabled=true;
 document.getElementById("b9").disabled=true;
-window.alert("player X won");
+document.getElementById('msg')
+            .innerHTML = "Player X won";
+            scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
 
     }   
    
-if((b2==="X")&&(b5==="X")&&(b8==="X")){
+else if((b2==="X")&&(b5==="X")&&(b8==="X")){
     document.getElementById("b1").disabled=true;
     document.getElementById("b3").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b6").disabled=true;
     document.getElementById("b7").disabled=true;
     document.getElementById("b9").disabled=true;
-    window.alert("player X won");
+    document.getElementById('msg')
+            .innerHTML = "Player X won";
+            scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
         }    
       
      
-if((b3==="X")&&(b6==="X")&&(b9==="X")){
+else if((b3==="X")&&(b6==="X")&&(b9==="X")){
     document.getElementById("b2").disabled=true;
     document.getElementById("b1").disabled=true;
     document.getElementById("b5").disabled=true;
     document.getElementById("b7").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b9").disabled=true;
-    window.alert("player X won");
+    document.getElementById('msg')
+            .innerHTML = "Player X won";
+            scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
         }   
     
-if((b1==="X")&&(b5==="X")&&(b9==="X")){
+else if((b1==="X")&&(b5==="X")&&(b9==="X")){
     document.getElementById("b2").disabled=true;
     document.getElementById("b3").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b6").disabled=true;
     document.getElementById("b8").disabled=true;
     document.getElementById("b7").disabled=true;
-    window.alert("player X won");
+    document.getElementById('msg')
+            .innerHTML = "Player X won";
+            scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
         }
-if((b3==="X")&&(b5==="X")&&(b7==="X")){
+else if((b3==="X")&&(b5==="X")&&(b7==="X")){
     document.getElementById("b2").disabled=true;
     document.getElementById("b1").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b6").disabled=true;
     document.getElementById("b8").disabled=true;
     document.getElementById("b9").disabled=true;
-    window.alert("player X won");
+    document.getElementById('msg')
+            .innerHTML = "Player X won";
+            scoreX=scoreX+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
         }  
 
 
-if((b1==="O")&&(b2==="O")&&(b3==="O")){
+else if((b1==="O")&&(b2==="O")&&(b3==="O")){
 document.getElementById("b4").disabled=true;
 document.getElementById("b5").disabled=true;
 document.getElementById("b6").disabled=true;
 document.getElementById("b7").disabled=true;
 document.getElementById("b8").disabled=true;
 document.getElementById("b9").disabled=true;
-window.alert("player O won");
+document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
+
 
 }
 
-if((b4==="O")&&(b5==="O")&&(b6==="O")){
+else if((b4==="O")&&(b5==="O")&&(b6==="O")){
 document.getElementById("b1").disabled=true;
 document.getElementById("b2").disabled=true;
 document.getElementById("b3").disabled=true;
 document.getElementById("b7").disabled=true;
 document.getElementById("b8").disabled=true;
 document.getElementById("b9").disabled=true;
-window.alert("player O won");
+document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
 
 }
 
-if((b7==="O")&&(b8==="O")&&(b9==="O")){
+else if((b7==="O")&&(b8==="O")&&(b9==="O")){
 document.getElementById("b1").disabled=true;
 document.getElementById("b2").disabled=true;
 document.getElementById("b3").disabled=true;
 document.getElementById("b4").disabled=true;
 document.getElementById("b5").disabled=true;
 document.getElementById("b6").disabled=true;
-window.alert("player O won");
+document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
 
     }   
 
-if((b1==="O")&&(b4==="O")&&(b7==="O")){
+else if((b1==="O")&&(b4==="O")&&(b7==="O")){
 document.getElementById("b2").disabled=true;
 document.getElementById("b3").disabled=true;
 document.getElementById("b5").disabled=true;
 document.getElementById("b6").disabled=true;
 document.getElementById("b8").disabled=true;
 document.getElementById("b9").disabled=true;
-window.alert("player O won");
+document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
 
 }   
 
-if((b2==="O")&&(b5==="O")&&(b8==="O")){
+else if((b2==="O")&&(b5==="O")&&(b8==="O")){
     document.getElementById("b1").disabled=true;
     document.getElementById("b3").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b6").disabled=true;
     document.getElementById("b7").disabled=true;
     document.getElementById("b9").disabled=true;
-    window.alert("player O won");
+    document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
         }    
 
 
-if((b3==="O")&&(b6==="O")&&(b9==="O")){
+else if((b3==="O")&&(b6==="O")&&(b9==="O")){
 document.getElementById("b2").disabled=true;
 document.getElementById("b1").disabled=true;
 document.getElementById("b5").disabled=true;
 document.getElementById("b7").disabled=true;
 document.getElementById("b4").disabled=true;
 document.getElementById("b9").disabled=true;
-window.alert("player O won");
+document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
 
     }   
 
-if((b1==="O")&&(b5==="O")&&(b9==="O")){
+else if((b1==="O")&&(b5==="O")&&(b9==="O")){
     document.getElementById("b2").disabled=true;
     document.getElementById("b3").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b6").disabled=true;
     document.getElementById("b8").disabled=true;
     document.getElementById("b7").disabled=true;
-    window.alert("player O won");
+    document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
         }
-if((b3==="O")&&(b5==="O")&&(b7==="O")){
+else if((b3==="O")&&(b5==="O")&&(b7==="O")){
     document.getElementById("b2").disabled=true;
     document.getElementById("b1").disabled=true;
     document.getElementById("b4").disabled=true;
     document.getElementById("b6").disabled=true;
     document.getElementById("b8").disabled=true;
     document.getElementById("b9").disabled=true;
-    window.alert("player O won");
+    document.getElementById('msg')
+            .innerHTML = "Player O won";
+            scoreY=scoreY+1;
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
     
-        }                            
+        }   
+    
+else if((b1==="X" || b1==="O") && (b2==="X" || b2==="O") && (b3==="X" || b3==="O") && (b4==="X" || b4==="O") && (b5==="X" || b5==="O") && (b6==="X" || b6==="O") && (b7==="X" || b7==="O") && (b8==="X" || b8==="O") && (b9==="X" || b9==="O")){
+    document.getElementById('msg')
+            .innerHTML = "Tie";
+document.getElementById('p1').innerHTML="player X score:"+ scoreX;
+            
+document.getElementById('p2').innerHTML="player O score:"+ scoreY;
+}
+else {}
+if(scoreX ===5){
+    document.getElementById('msg')
+    .innerHTML = "Player X won the game";  
+    document.getElementById("b1").disabled=true;
+    document.getElementById("b2").disabled=true;
+    document.getElementById("b3").disabled=true;
+    document.getElementById("b4").disabled=true;
+    document.getElementById("b5").disabled=true;
+    document.getElementById("b6").disabled=true;
+    document.getElementById("b7").disabled=true;
+    document.getElementById("b8").disabled=true;
+    document.getElementById("b9").disabled=true;
+    document.getElementById("reset").disabled=true;
+}
+if(scoreY ===5){
+    document.getElementById('msg')
+    .innerHTML = "Player O won the game";  
+    document.getElementById("b1").disabled=true;
+    document.getElementById("b2").disabled=true;
+    document.getElementById("b3").disabled=true;
+    document.getElementById("b4").disabled=true;
+    document.getElementById("b5").disabled=true;
+    document.getElementById("b6").disabled=true;
+    document.getElementById("b7").disabled=true;
+    document.getElementById("b8").disabled=true;
+    document.getElementById("b9").disabled=true;
+    document.getElementById("reset").disabled=true;
+}
+
+
+}
+
+function reset_fun(){
+    //location.reload();
+    document.getElementById("b1").value="";
+    document.getElementById("b2").value="";
+    document.getElementById("b3").value="";
+    document.getElementById("b4").value="";
+    document.getElementById("b5").value="";
+    document.getElementById("b6").value="";
+    document.getElementById("b7").value="";
+    document.getElementById("b8").value="";
+    document.getElementById("b9").value="";
+    document.getElementById("b1").disabled=false;
+    document.getElementById("b2").disabled=false;
+    document.getElementById("b3").disabled=false;
+    document.getElementById("b4").disabled=false;
+    document.getElementById("b5").disabled=false;
+    document.getElementById("b6").disabled=false;
+    document.getElementById("b7").disabled=false;
+    document.getElementById("b8").disabled=false;
+    document.getElementById("b9").disabled=false;
+    player=1;
+    document.getElementById("msg").innerHTML="";
 
 }
